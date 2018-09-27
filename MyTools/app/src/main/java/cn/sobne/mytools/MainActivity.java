87c07357a151ef59fe849e90542d8a11618bec2d
sobne <sobne.cn@gmail.com>
2018-09-27 20:03:01 +0800

    mainactivity

--- MyTools/app/src/main/java/cn/sobne/mytools/MainActivity.java
+++ MyTools/app/src/main/java/cn/sobne/mytools/MainActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import cn.sobne.myconvertor.ConvertorActivity;
 
@@ -18,20 +24,61 @@ public class MainActivity extends AppCompatActivity {
 
         init();
     }
-    private void init()
-    {
-        mConvertor =(Button)findViewById(R.id.btnConvertor);
-
-        mConvertor.setOnClickListener(clickListener);
-    }
     private View.OnClickListener clickListener=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()){
-                case R.id.btnConvertor:{
-                    startActivity(new Intent(MainActivity.this, ConvertorActivity.class));
-                }
+//                case R.id.btnConvertor:{
+//                    startActivity(new Intent(MainActivity.this, ConvertorActivity.class));
+//                }
             }
         }
     };
+    private void init()
+    {
+//        mConvertor =(Button)findViewById(R.id.btnConvertor);
+//        mConvertor.setOnClickListener(clickListener);
+
+        GridView gridview = (GridView) findViewById(R.id.gvmain);
+        ArrayList<HashMap<String, Object>> lstImageItem=getImageItems();
+        SimpleAdapter saImageItems = new SimpleAdapter(this,
+                lstImageItem,
+                R.layout.item,
+                new String[]{"ItemImage", "ItemText"},
+                new int[]{R.id.img_convertor, R.id.txt_convertor});
+        gridview.setAdapter(saImageItems);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position)
+                {
+                    case 0:
+                        startActivity(new Intent(MainActivity.this, ConvertorActivity.class));
+                        break;
+                }
+            }
+        });
+
+    }
+
+    private ArrayList<HashMap<String, Object>> getImageItems()
+    {
+        int[] imageRes = {
+                R.drawable.convertor};
+
+        String[] name = {
+                "进制转换"
+        };
+        int length = imageRes.length;
+
+        ArrayList<HashMap<String, Object>> lstImageItem = new ArrayList<HashMap<String, Object>>();
+        for (int i = 0; i < length; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImage", imageRes[i]);
+            map.put("ItemText", name[i]);
+            lstImageItem.add(map);
+        }
+        return lstImageItem;
+    }
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MyTools
+20180927  MyConvertor using gridview in main activity
 20180926  MyConvertor
